614a4242f5897226068d31bf41d555de959f0aa0
Acewhiteowl <144960401+Acewhiteowl@users.noreply.github.com>
2023-11-19 14:42:52 -0500

    Add files via upload

--- src/machine_hiding/file_system_operation/file_permission.rs
+++ src/machine_hiding/file_system_operation/file_permission.rs
@@ -1,11 +1,50 @@
-pub mod machine_hiding{
-    pub mod file_system_operation{
-        pub mod file_permission{
-            pub fn get_permissions(){         
+pub mod Machine_Hiding{
+    pub mod File_System_Operation{
+        pub mod  File_Permission{
+            use crate::machine_hiding::os_detection::Machine_Hiding::OS_Detection::detect_OS;
+            use std::fs;
+            pub fn get_permissions(file_path: &str) -> std::io::Result<fs::Permissions>{        
+                let os=detect_OS();
+                match os {
+                    "linux" => {
+                        println!("Running on Linux");
+                        fs::metadata(file_path).map(|metadata| metadata.permissions()) 
+                    },
+                    "windows" => {
+                        println!("Running on Windows");
+                        // Windows 
+                    },
+                    "macos" => {
+                        println!("Running on macOS");
+                        // macOS 
+                    },
+                    _ => {
+                        println!("Running on an unknown or unsupported OS: {}", os);
+                        // other
+                    }
+                }
             }
 
-            pub fn set_permissions(){
-
+            pub fn set_permissions(file_path: &str, permissions: fs::Permissions) -> std::io::Result<()>{
+                let os=detect_OS();
+                match os {
+                    "linux" => {
+                        println!("Running on Linux");
+                        fs::set_permissions(file_path, permissions) 
+                    },
+                    "windows" => {
+                        println!("Running on Windows");
+                        // Windows 
+                    },
+                    "macos" => {
+                        println!("Running on macOS");
+                        // macOS 
+                    },
+                    _ => {
+                        println!("Running on an unknown or unsupported OS: {}", os);
+                        // other
+                    }
+                }
             }
         }
     }
