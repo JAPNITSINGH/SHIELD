960ca843435acb1a727178ff236497a358731740
easyflyjin <yjin43@ur.rochester.edu>
2023-12-09 15:30:42 -0500

    found a bug

--- src/behaviour_hiding/input.rs
+++ src/behaviour_hiding/input.rs
@@ -215,6 +215,6 @@ fn process_clone(args: Vec<&str>){
         if let Err(e) = file_basic::clone(args[2]) {
             println!("Failed to clone the repository: {}", e);
         }
-        println!("The repository is successfully cloned!")
+        println!("The repository is successfully cloned!");
     }
 }
