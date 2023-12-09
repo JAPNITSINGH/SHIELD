726dfdedfde7737ea901af87b389279743726177
Acewhiteowl <kqi2@ur.rochester.edu>
2023-12-09 18:06:42 -0500

    func pull

--- src/machine_hiding/file_log.rs
+++ src/machine_hiding/file_log.rs
@@ -2,6 +2,12 @@
 use sha1::{Sha1, Digest};
 use std::time::{SystemTime, UNIX_EPOCH};
 
+
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::io::{self, ErrorKind};
+
+
 pub fn generate_hash_id(filename: &String) -> String {
     let now = SystemTime::now();
     let since_the_epoch = now.duration_since(UNIX_EPOCH)
@@ -15,6 +21,28 @@ pub fn generate_hash_id(filename: &String) -> String {
 
     let hash = hasher.finalize();
     return format!("{:x}", hash)
+}
+
+pub fn pull(from: &str, to: &str) -> io::Result<()> {
+
+    let from_dir=Path::new(from);
+    let to_dir=Path::new(to);
+
+    if !from_dir.is_dir() {
+        return Err(io::Error::new(ErrorKind::NotFound, "Source directory does not exist"));
+    }
+    if !to_dir.is_dir() {
+        fs::create_dir_all(to_dir)?;
+    }
+    fs::read_dir(from_dir)?
+        .filter_map(Result::ok)  
+        .map(|entry| entry.path())
+        .filter(|path| path.is_file())
+        .try_for_each(|path| {
+            let dest_path = to_dir.join(path.file_name().ok_or_else(|| io::Error::new(ErrorKind::Other, "Missing file name"))?);
+            fs::copy(&path, &dest_path)?;
+            Ok(())
+        })
 }
 
 
