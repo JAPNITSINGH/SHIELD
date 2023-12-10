25f04eefee9b60c1cfc5ca4fa5dbd92768b34ca3
Acewhiteowl <kqi2@ur.rochester.edu>
2023-12-10 18:42:18 -0500

    pull/push

--- src/repository_hiding/repository_origin.rs
+++ src/repository_hiding/repository_origin.rs
@@ -1,6 +1,9 @@
 use std::fs;
-use std::path::Path;
 use std::io;
+
+use std::path::{Path, PathBuf};
+use std::io::{self, ErrorKind};
+use walkdir::WalkDir;
 
 use crate::machine_hiding::{os_detection,file_system_operation::file_basic};
 
@@ -43,4 +46,62 @@ pub fn init_main() {
         Ok(_) => println!("Repository initialized"),
         Err(e) => println!("Failed to initialize repository: {}", e),
     }
+}
+
+pub fn pull(from: &str, to: &str) -> io::Result<()> {
+    let from_dir=Path::new(from);
+    let to_dir=Path::new(to);
+
+    if !from_dir.is_dir() {
+        return Err(io::Error::new(ErrorKind::NotFound, "Source directory does not exist"));
+    }
+    if !to_dir.is_dir() {
+        fs::create_dir_all(to_dir)?;
+    }
+    WalkDir::new(from_dir)
+        .into_iter()
+        .filter_map(Result::ok)
+        .map(|entry| entry.into_path())
+        .try_for_each(|path| {
+            let relative_path = path.strip_prefix(from_dir).unwrap_or(&path);
+            let dest_path = to_dir.join(relative_path);
+            if path.is_dir() {
+                fs::create_dir_all(&dest_path)
+            } else {
+                if let Some(parent) = dest_path.parent() {
+                    fs::create_dir_all(parent)?;
+                }
+                fs::copy(&path, &dest_path)?;
+                Ok(())
+            }
+        })
+}
+
+pub fn push(from: &str, to: &str) -> io::Result<()> {
+    let from_dir=Path::new(from);
+    let to_dir=Path::new(to);
+
+    if !from_dir.is_dir() {
+        return Err(io::Error::new(ErrorKind::NotFound, "Source directory does not exist"));
+    }
+    if !to_dir.is_dir() {
+        fs::create_dir_all(to_dir)?;
+    }
+    WalkDir::new(from_dir)
+        .into_iter()
+        .filter_map(Result::ok)
+        .map(|entry| entry.into_path())
+        .try_for_each(|path| {
+            let relative_path = path.strip_prefix(from_dir).unwrap_or(&path);
+            let dest_path = to_dir.join(relative_path);
+            if path.is_dir() {
+                fs::create_dir_all(&dest_path)
+            } else {
+                if let Some(parent) = dest_path.parent() {
+                    fs::create_dir_all(parent)?;
+                }
+                fs::copy(&path, &dest_path)?;
+                Ok(())
+            }
+        })
 }
