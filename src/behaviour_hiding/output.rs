f4741742ca3a49c8a9dbc4d2af9d39c9fb735a22
easyflyjin <yjin43@ur.rochester.edu>
2023-12-10 21:31:32 -0500

    help ult

--- src/behaviour_hiding/output.rs
+++ src/behaviour_hiding/output.rs
@@ -10,18 +10,25 @@ These are shield commands commonly used:
 - Initialize a new repository:   shield init
 - Clone an existing repository:  shield clone <\"repo_url\">
 - Check your current status:    shield status
-- Add files to staging:         shield add <\"file_name \">
+- Add files to staging:         shield add
 - Commit your changes:          shield commit <\"your message\">
-- Push changes to remote:       shield push
-- Pull updates from remote:     shield pull
-- Remove a file from repository:  shield remove <\"file_name\">
+- Push changes to remote:       shield push <\"target path\">
+- Pull updates from remote:     shield pull <\"target path\">
+- Remove a file from the directory:  shield rm <\"file_name\">
 - Move a file to a different directory: shield mv <\"file_name\"> <\"new_path\">
 - Check the files and folders:  shield ls
 - Move to a specific directory      shield cd <\"folder_name\">
 - Create a file in the repo:     shield createfile <\"file_name\">
 - Create a folder in the repo:   shield createfolder <\"folder_name\">
 - Write a line in a file:        shield write <\"file_name\"> <\"content\">
-- Read a file content:           shield cat <\"file_name\">  
+- Read a file content:           shield cat <\"file_name\"> 
+- Check the diff of different commit:      shield diff <\"(backslash\\)file name\"> <\"commit id\">
+- Check the heads:                shield heads
+- Check the log:                  shield log
+- Checkout to a different branch    shield checkout <\"branch name\">
+- Switch to a different branch in the cloned repository:   shield branch <\"branch name\">
+- Merge code from 2 branches:        shield merge  <\"branch_name\">
+
             "
     );
 }
